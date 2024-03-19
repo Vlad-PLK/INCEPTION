2fa334a4d4eabe22270da698d8132e9e8e9d7f31
Vlad-PLK <leonpolo365@gmail.com>
2024-03-19 17:48:11 +0100

    script ok

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-service mysql start
+service mariadb start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysql -e "CREATE DATABASE IF NOT EXISTS db;"
+mysql -e "CREATE USER IF NOT EXISTS vld@'localhost' IDENTIFIED BY 'plk';"
+mysql -e "GRANT ALL PRIVILEGES ON db.* TO vld@'%' IDENTIFIED BY 'plk';"
+mysql -e "ALTER USER root@'localhost' IDENTIFIED BY 'vldplk';"
+mysql -e "FLUSH PRIVILEGES;" -u root -pvldplk
+mysqladmin -u root -pvldplk shutdown
 exec mysqld_safe
