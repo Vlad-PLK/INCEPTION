2a5303aa37ea8b9e03355b72b3d3bdf61af803dc
Vlad-PLK <leonpolo365@gmail.com>
2024-03-20 13:03:23 +0100

    working script for debian buster

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-service mariadb start
+mysql_install_db
+service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS db;"
-mysql -e "CREATE USER IF NOT EXISTS vld@'localhost' IDENTIFIED BY 'plk';"
-mysql -e "GRANT ALL PRIVILEGES ON db.* TO vld@'%' IDENTIFIED BY 'plk';"
-mysql -e "ALTER USER root@'localhost' IDENTIFIED BY 'vldplk';"
-mysql -e "FLUSH PRIVILEGES;" -u root -pvldplk
-mysqladmin -u root -pvldplk shutdown
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e 'FLUSH PRIVILEGES;'
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
