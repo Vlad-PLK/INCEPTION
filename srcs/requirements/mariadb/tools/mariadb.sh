058eeb47e9062a570dd44defbd8aaa3e54c9f580
Vlad-PLK <leonpolo365@gmail.com>
2024-03-29 10:38:16 +0100

    still fixing mariadb script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+MYSQL_DB_NAME="mariadb"
+MYSQL_ROOT_PSW="vlplk"
+MYSQL_USER="vlad"
+MYSQL_PSW="vld"
+
 service mariadb start
 
 touch db.sql
 cat << EOF > db.sql
+FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB_NAME}\`;
 CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PSW}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DB_NAME}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PSW}';
-ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PSW}';
-FLUSH PRIVILEGES;
+ALTER USER root@localhost IDENTIFIED BY '${MYSQL_ROOT_PSW}';
 EOF
 
 mariadb < db.sql
 
-mysqladmin -u root -p'vldplk' shutdown
+mysqladmin -u root -p${MYSQL_ROOT_PSW} shutdown
--- srcs/requirements/mariadb/tools/mariadb_c.sh
+++ srcs/requirements/mariadb/tools/mariadb_c.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
-service mariadb start
+MYSQL_DB_NAME="mariadb"
+MYSQL_ROOT_PSW="vlplk"
+MYSQL_USER="vlad"
+MYSQL_PSW="vld"
 
-touch db.sql
-cat << EOF > db.sql
-CREATE DATABASE IF NOT EXISTS mdb;
-CREATE USER IF NOT EXISTS plk@'localhost' IDENTIFIED BY 'vld';
-GRANT ALL PRIVILEGES ON mdb.* TO plk@'%' IDENTIFIED BY 'vld';
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'vldplk';
-FLUSH PRIVILEGES;
-EOF
+echo "FLUSH PRIVILEGES;" | mariadbd --bootstrap --user=root
+echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB_NAME}\`;" | mariadbd --bootstrap --user=root
+echo "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PSW}';" | mariadbd --bootstrap --user=root
+echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DB_NAME}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PSW}';" | mariadbd --bootstrap --user=root
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PSW}';" | mariadbd --bootstrap --user=root
 
-mariadb < db.sql
+kill %1
 
-mysqladmin -u root -p'vldplk' shutdown
+mariadbd --bootstrap --user=root & 
+sleep 3
+##service mariadb start
+##
+##touch db.sql
+##cat << EOF > db.sql
+##CREATE DATABASE IF NOT EXISTS mdb;
+##CREATE USER IF NOT EXISTS plk@'localhost' IDENTIFIED BY 'vld';
+##GRANT ALL PRIVILEGES ON mdb.* TO plk@'%' IDENTIFIED BY 'vld';
+##ALTER USER 'root'@'localhost' IDENTIFIED BY 'vldplk';
+##FLUSH PRIVILEGES;
+##EOF
+##
+##mariadb < db.sql
+##
+##mysqladmin -u root -p'vldplk' shutdown
+##
