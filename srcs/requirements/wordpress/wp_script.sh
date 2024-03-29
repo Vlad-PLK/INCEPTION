b6fcb2a5708ff232e1776ab561e74da95e815fe2
Vlad-PLK <leonpolo365@gmail.com>
2024-03-29 16:47:53 +0100

    :(

--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 10
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
@@ -8,19 +10,33 @@ mv wp-cli.phar /usr/local/bin/wp
 mkdir /var/www
 mkdir /var/www/html
 mkdir /var/www/html/wordpress
+chmod -R 775 /var/www/html/wordpress
+chmod -R www-data /var/www/html/wordpress
 
 cd /var/www/html/wordpress
 
-wp core download --allow-root
-
-sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-sed -i "s/password_here/$MYSQL_PSW/g" wp-config-sample.php
-sed -i "s/localhost/mariadb/g" wp-config-sample.php
-sed -i "s/database_name_here/mariadb/g" wp-config-sample.php
+wp core download --allow-root --path=/var/www/html/wordpress
 
 mv wp-config-sample.php wp-config.php
 
-wp core install --url=$DOMAIN_NAME --title=42 --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PSW --admin_email=$WP_ADMIN.42.fr --skip-email --allow-root
-wp user create $WP_USER --user_pass=$WP_USER_PSW --role=author --allow-root
+sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
+sed -i "s/password_here/$MYSQL_PSW/g" wp-config.php
+sed -i "s/localhost/$MYSQL_DB_NAME/g" wp-config.php
+sed -i "s/database_name_here/mariadb:3306/g" wp-config.php
+
+wp core install --allow-root \
+        --url=$DOMAIN_NAME \
+        --title=inception \
+        --admin_user=$WP_ADMIN \
+        --admin_password=$WP_ADMIN_PSW \
+        --admin_email=inception.42.fr \
+        --skip-email \
+        --path=/var/www/html/wordpress
+wp user create \
+    $WP_USER \
+    --user_pass=$WP_USER_PSW \
+    --role=author \
+    --allow-root \
+    --path=/var/www/html/wordpress \
 
 mkdir /run/php
